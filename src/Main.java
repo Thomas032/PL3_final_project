71c0ca105820978b39b5881eeaeab2b9527fb979
Thomas032 <80817250+Thomas032@users.noreply.github.com>
2025-11-22 18:54:56 +0200

    feat: implement solution for binary search

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         MenuBuilder menu = new MenuBuilder();
 
         menu.addMenuOption("Linear searching", Main::performLinearSearch);
-        menu.addMenuOption("Binary searching", () -> System.out.println("Bin search"));
+        menu.addMenuOption("Binary searching", Main::performBinarySearch);
         menu.addMenuOption("O(n^2) type of sorting", () -> System.out.println("Exp sort"));
         menu.addMenuOption("O(n*log(n)) type of sorting", () -> System.out.println("Ln sort"));
         menu.addMenuOption("Sorting performance", () -> System.out.println("Cmp"));
@@ -21,7 +21,7 @@ public class Main {
     private static void performLinearSearch(){
         final int upperBound = 9;
         final int lowerBound = 0;
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         for (int i = lowerBound; i <= upperBound; i++){
             list.add(i);
         }
@@ -44,7 +44,45 @@ public class Main {
         } catch (NumberFormatException e) {
             System.out.println("Invalid input. Try again.");
         }
+    }
 
+    private static void performBinarySearch(){
+        final int upperBound = 9;
+        final int lowerBound = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = lowerBound; i <= upperBound; i++){
+            list.add(i);
+        }
 
+        System.out.printf("In the list are values %d, ..., %d\n", lowerBound, upperBound);
+        System.out.print("Which value would you like to search with linear search? ");
+
+        try{
+            String val = input.nextLine();
+            int element = Integer.parseInt(val);
+
+            int l = 0;
+            int r = list.size()-1;
+
+            while(l <= r){
+                int middle = (l+r)/2;
+
+                if(list.get(middle) == element){
+                    System.out.println("\nFound");
+                    return;
+                }
+
+                if(list.get(middle) > element){
+                    r = middle - 1;
+                }
+                else{
+                    l = middle + 1;
+                }
+            }
+
+            System.out.println("\nNot found");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Try again.");
+        }
     }
 }
